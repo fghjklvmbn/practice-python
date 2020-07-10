2a08b4e9df81647f771ba3c7f876d9e7fa58c3aa
fghjklvmbn <66707337+fghjklvmbn@users.noreply.github.com>
2020-07-10 12:21:17 +0900

    practice

--- practice.py
+++ practice.py
@@ -63,4 +63,46 @@ print(number)
 number /= 3
 print(number)
 number *= 12
+print(number)
+number %= 3
+print(number)
 
+print(abs(-5))
+print(pow(4, 2))
+print(max(5, 12))
+print(min(5, 2))
+print(round(3.14)) #반올림
+print(round(4.99)) #반올림
+
+from math import *
+print(floor(4.99)) #내림
+print(ceil(3.14)) #올림
+print(sqrt(16)) #제곱근 
+
+from random import *
+
+print(random()) #0.0~1.0 미만의 임의의 값을 생성
+print(random() * 10) #0부터 10까지의 임의의 값을 생성
+print(int(random() * 10)) #0부터 10까지의 소수점 없는 임의의 값을 생성
+print(int(random() * 10))
+print(int(random() * 10))
+print(int(random() * 10))
+print(int(random() * 10 ) + 1) # 1부터 10까지의 소수점 없는 임의의 값을 생성
+print(int(random() * 10 ) + 1)
+print(int(random() * 10 ) + 1)
+
+print(randrange(1, 46)) #1부터 45까지의 소수점 없는 임의의 값을 생성
+print(randrange(1, 46)) #1부터 45까지의 소수점 없는 임의의 값을 생성
+print(randrange(1, 46)) #1부터 45까지의 소수점 없는 임의의 값을 생성
+print(randrange(1, 46)) #1부터 45까지의 소수점 없는 임의의 값을 생성
+print(randrange(1, 46)) #1부터 45까지의 소수점 없는 임의의 값을 생성
+
+print(randint(1,34)) #1부터 34이하의 소수점 없는 임의의 값을 생성
+print(randint(1,34)) #1부터 34이하의 소수점 없는 임의의 값을 생성
+print(randint(1,34)) #1부터 34이하의 소수점 없는 임의의 값을 생성
+print(randint(1,34)) #1부터 34이하의 소수점 없는 임의의 값을 생성
+print(randint(1,34)) #1부터 34이하의 소수점 없는 임의의 값을 생성
+print(randint(1,34)) #1부터 34이하의 소수점 없는 임의의 값을 생성
+
+a = randint(4, 28)
+print("오프라인 스터디 모임 날짜는 매월 {}일로 선정되었습니다.".format(a))
---
+++ practice2.py
@@ -0,0 +1,5 @@
+from random import randint
+a = randint(4,28)
+print("오프라인 스터디 모임 날짜는 매월 {}일로 선정되었습니다.".format(a))
+'''랜덤 함수를 끌어와야 비로소 randint나 randrange등
+random 관련한 모든 기능이 작동한다.'''
