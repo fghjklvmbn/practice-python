31944b0925a4eea13173f323ec470c788b72861b
fghjklvmbn <66707337+fghjklvmbn@users.noreply.github.com>
2020-09-24 19:21:11 +0900

    Update opencv2.py

--- opencv/opencv2.py
+++ opencv/opencv2.py
@@ -1,12 +1,11 @@
 import numpy as np
 import cv2
 
-def showimage():
-    imgfile = "images/과제.PNG"
-    img = cv2.imread(imgfile, cv2.UNCHANGED)
+imgfile = "images/과제.PNG"
+img = cv2.imread(imgfile, cv2.RGBT2GREY)
 
-    cv2.imshow('과제', img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+cv2.imshow('과제', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 showimage()
