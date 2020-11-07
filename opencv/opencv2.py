8dededb3e816de51899dd1d7157d554cf3865cd4
fghjklvmbn <66707337+fghjklvmbn@users.noreply.github.com>
2020-11-07 18:56:11 +0900

    opencv and others

--- opencv/opencv2.py
+++ opencv/opencv2.py
@@ -1,11 +1,7 @@
-import numpy as np
 import cv2
 
-imgfile = "images/과제.PNG"
-img = cv2.imread(imgfile, cv2.RGBT2GREY)
+img = cv2.imread("test/kid.png", 1)
 
-cv2.imshow('과제', img)
+cv2.imshow("park", img)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
 
-showimage()
